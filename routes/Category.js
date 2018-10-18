0ed5da8c2cc1b13de235803bb39cf4713fe425bb
Brendan <brendan.bobis@epitech.eu>
2018-10-18 12:04:38 +0200

    Ajout d'une requette pour les stats de la plateforme

--- routes/Category.js
+++ routes/Category.js
@@ -27,6 +27,30 @@ module.exports = (models) => {
     return response.success_response(h, result, null, 200);
   };
 
+  /**
+   * @api {get} /api/v1/categories/statistics Get photo number by categories
+   * @apiName CategoriesStats
+   * @apiGroup Category
+   * @apiVersion 1.0.0
+   *
+   * @apiHeader (Header fields required) {X-API-KEY} X-API-KEY The api token value [required]
+   * @apiHeader (Header fields required) {Content-Type} Content-Type Must be application/json
+   * @apiHeaderExample {header} X-API-KEY
+   * X-API-KEY: your_token...
+   * @apiHeaderExample {header} Content-Type
+   * Content-Type: application/json
+   *
+   */
+  const getStats = async (request, h) => {
+    const stats = await models.category.findAll({
+      attributes: Object.keys(models.category.attributes).concat([
+        [models.sequelize.literal('(SELECT COUNT(*) FROM photos WHERE photos.category_id = category.id)'), 'total_photos'],
+      ]),
+    });
+
+    return response.success_response(h, stats, null, 200);
+  };
+
   return [
     {
       method: 'GET',
@@ -34,6 +58,14 @@ module.exports = (models) => {
       config: {
         auth: 'default',
         handler: getCategories,
+      },
+    },
+    {
+      method: 'GET',
+      path: '/categories/statistics',
+      config: {
+        auth: 'default',
+        handler: getStats,
       },
     },
   ];
